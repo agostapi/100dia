8eb7888ffd90532768d90519d317b2a4a67122bb
Illes <Kisanna.Illes@t-systems.com>
2016-11-27 20:30:12 +0100

    asdf

--- teszt_design_git.py
+++ teszt_design_git.py
@@ -18,7 +18,7 @@ from pptx.util import Cm
 from pptx.util import Inches
 from pptx.util import Pt
 from pptx.dml.line import LineFormat
-#from pptx.dml.chtfmt import ChartFormat
+from pptx.dml.chtfmt import ChartFormat
 from pptx.dml.fill import FillFormat
 from pptx.dml.color import RGBColor
 from pptx.util import Pt
@@ -64,7 +64,7 @@ def fill_slide_1(tsg_ppt, org_names, org_percents):
   chart_data = ChartData()
   chart_data.categories= org_names
   series=chart_data.add_series('01', org_percents)
-  x,y,cx,cy = Inches(0.3), Inches(2.0), Inches(9.38), Inches(4.7)
+  x,y,cx,cy = Inches(0.3), Inches(2.0), Inches(5.38), Inches(3)
   graphic_frame = slide.shapes.add_chart(XL_CHART_TYPE.BAR_CLUSTERED, x, y, cx, cy, chart_data)
   chart = graphic_frame.chart
   value_axis = chart.value_axis
@@ -87,14 +87,18 @@ def fill_slide_1(tsg_ppt, org_names, org_percents):
   category_axis.has_major_gridlines = False
   value_axis.has_major_gridlines = False
   value_axis.visible = False
-  #series.fill.solid()
-  #series.fill.fore_color.rgb=RGBColor(0,0,0)
+  #bar_plot.fill.solid()
+  #bar_plot.fill.fore_color.rgb=RGBColor(0,0,0)
   return tsg_ppt
 #fills the second slide with 9 charts of the 9 questions, appears a table by the first chart with the diffrence to the last years data and with an arrow which signs the way of the difference
 def fill_slide_2(tsg_ppt, q1):
   slide = tsg_ppt.slides[2]
   chart_data = ChartData()
+  chart_data_2 =  ChartData()
+  chart_data_3 =  ChartData()
   chart_data.categories=['1', '2']
+  chart_data_2.categories=['1', '2']
+  chart_data_3.categories=['1', '2', '3']
   a = [30,40,30]
   b = [20,30, 50]
   c = [20,59.4,20.6]
@@ -105,12 +109,68 @@ def fill_slide_2(tsg_ppt, q1):
   chart_data.add_series('2',(a[0], b[0]))#, 0, 0))#, b[0], c[0], d[0], e[0], f[0], g[0])) #, d[0]))
   chart_data.add_series('3',(a[1], b[1]))#, 0, 0))#, b[1], c[1], d[1], e[1], f[1], g[1]))
   chart_data.add_series('4',(a[2], b[2]))#, 0, 0))#, b[2], c[2], d[2], e[2], f[2], g[2]))#, d[2]))
-  x,y,cx,cy = Inches(0.3), Inches(2.00), Inches(5.38), Inches(4.7)
+  chart_data_2.add_series('2',(c[0], d[0]))
+  chart_data_2.add_series('3',(c[1], d[1]))
+  chart_data_2.add_series('4',(c[2], d[2]))
+  chart_data_3.add_series('2',(e[0], f[0], g[0]))
+  chart_data_3.add_series('2',(e[1], f[1], g[1]))
+  chart_data_3.add_series('2',(e[2], f[2], g[2]))
+  x,y,cx,cy = Cm(10.5), Cm(4.5), Cm(5.2), Cm(3)
   graphic_frame = slide.shapes.add_chart(XL_CHART_TYPE.BAR_STACKED_100, x, y, cx, cy, chart_data)
   chart = graphic_frame.chart
   value_axis = chart.value_axis
   chart.plots[0].has_data_labels = True
   data_labels = chart.plots[0].data_labels
+  bar_plot = chart.plots[0]
+  bar_plot.gap_width = 10
+  #nehasznaaalddata_labels.position = XL_LABEL_POSITION.OUTSIDE_END
+  chart.has_legend = False
+  data_labels.font.size = Pt(12)
+  data_labels.number_format = '0'
+  data_labels.font.color.rgb = RGBColor(0, 0, 0)
+  category_axis = chart.category_axis
+  category_axis.minor_tick_mark = XL_TICK_MARK.OUTSIDE
+  category_axis.tick_labels.font.size = Pt(12)
+  value_axis.minor_tick_mark = XL_TICK_MARK.NONE
+  category_axis.major_tick_mark = XL_TICK_MARK.NONE
+  value_axis.major_tick_mark = XL_TICK_MARK.NONE
+  category_axis.has_major_gridlines = False
+  value_axis.has_major_gridlines = False
+  value_axis.visible = False
+  category_axis.visible = False
+
+  x,y,cx,cy = Cm(10.5), Cm(6.68), Cm(5.2), Cm(3)
+  graphic_frame = slide.shapes.add_chart(XL_CHART_TYPE.BAR_STACKED_100, x, y, cx, cy, chart_data_2)
+  chart = graphic_frame.chart
+  value_axis = chart.value_axis
+  chart.plots[0].has_data_labels = True
+  data_labels = chart.plots[0].data_labels
+  bar_plot = chart.plots[0]
+  bar_plot.gap_width = 50
+  #nehasznaaalddata_labels.position = XL_LABEL_POSITION.OUTSIDE_END
+  chart.has_legend = False
+  data_labels.font.size = Pt(12)
+  data_labels.number_format = '0'
+  data_labels.font.color.rgb = RGBColor(0, 0, 0)
+  category_axis = chart.category_axis
+  category_axis.minor_tick_mark = XL_TICK_MARK.OUTSIDE
+  category_axis.tick_labels.font.size = Pt(12)
+  value_axis.minor_tick_mark = XL_TICK_MARK.NONE
+  category_axis.major_tick_mark = XL_TICK_MARK.NONE
+  value_axis.major_tick_mark = XL_TICK_MARK.NONE
+  category_axis.has_major_gridlines = False
+  value_axis.has_major_gridlines = False
+  value_axis.visible = False
+  category_axis.visible = False
+
+  x,y,cx,cy = Cm(10.5), Cm(8.86), Cm(5.2), Cm(4.1)
+  graphic_frame = slide.shapes.add_chart(XL_CHART_TYPE.BAR_STACKED_100, x, y, cx, cy, chart_data_3)
+  chart = graphic_frame.chart
+  value_axis = chart.value_axis
+  chart.plots[0].has_data_labels = True
+  data_labels = chart.plots[0].data_labels
+  bar_plot = chart.plots[0]
+  bar_plot.gap_width = 50
   #nehasznaaalddata_labels.position = XL_LABEL_POSITION.OUTSIDE_END
   chart.has_legend = False
   data_labels.font.size = Pt(12)
@@ -127,50 +187,36 @@ def fill_slide_2(tsg_ppt, q1):
   value_axis.visible = False
   category_axis.visible = False
   #chart.replace_data(chart_data)
+  
+  
+  rows=1
+  cols=1
+  left = Inches(5.83)
+  top = Inches(1.93)
+  width = Inches(0.8)
+  height = Inches (0.425)# set column widths
+  table = slide.shapes.add_table(rows, cols, left, top, width, height).table
+  table.columns[0].width = Inches(0.39)
+  table.cell(0, 0).text = "+3"
+  cell = table.rows[0].cells[0]
+  paragraph = cell.textframe.paragraphs[0]
+  paragraph.font.size = Pt(12)
+  paragraph.font.color.rgb = RGBColor(255, 255, 255)
+  cell.horizontal_anchor = MSO_ANCHOR.MIDDLE
+  cell.vertical_anchor = MSO_ANCHOR.MIDDLE
+  cell.fill.solid()
+  cell.fill.fore_color.rgb = RGBColor(124,124,124)
+  img_path = 'pirosnyil.png'
+  left = Inches(6.25)
+  height = Inches(0.3)
+  top = Inches(2)
+  pic = slide.shapes.add_picture(img_path, left, top, height=height)
   return tsg_ppt
 
 
-def fill_slide_common(tsg_ppt, x):
-  common_slide=tsg_ppt.slides[x]
-  chart_data = ChartData()
-  chart_data.categories=['1']
-  a = [30,40,30] 
-  b = [20,30, 50]
-  c = [20,59.4,20.6]
-  chart_data.add_series('2',(a[0], b[0], c[0]))#, d[0]))
-  chart_data.add_series('3',(a[1], b[1], c[1]))#, d[1]))
-  chart_data.add_series('4',(a[2], b[2], c[2]))#, d[2]))
-  x,y,cx,cy = Inches(0.3), Inches(2.00), Inches(5.38), Inches(4.7)
-  graphic_frame = common_slide.shapes.add_chart(XL_CHART_TYPE.BAR_STACKED_100, x, y, cx, cy, chart_data)
-  chart = graphic_frame.chart
-  value_axis = chart.value_axis
-  chart.plots[0].has_data_labels = True
-  data_labels = chart.plots[0].data_labels
-  #nehasznaaalddata_labels.position = XL_LABEL_POSITION.OUTSIDE_END
-  chart.has_legend = False
-  data_labels.font.size = Pt(12)
-  data_labels.number_format = '0'
-  data_labels.font.color.rgb = RGBColor(0, 0, 0)
-  category_axis = chart.category_axis
-  category_axis.minor_tick_mark = XL_TICK_MARK.OUTSIDE
-  category_axis.tick_labels.font.size = Pt(12)
-  value_axis.minor_tick_mark = XL_TICK_MARK.NONE
-  category_axis.major_tick_mark = XL_TICK_MARK.NONE
-  value_axis.major_tick_mark = XL_TICK_MARK.NONE
-  category_axis.has_major_gridlines = False
-  value_axis.has_major_gridlines = False
-  value_axis.visible = False
-  #chart.replace_data(chart_data)
-  return tsg_ppt
-
-def chartfromarray(tsg_ppt, tomb1, tomb2):
-  chart_data = ChartData()
-  chart_data.categories=tomb1
-  for i in tomb1:
-    chart_data.add_series('valami',tomb2[i])
 def difference_in_shape(tsg_ppt):
   gesamt_slide = tsg_ppt.slides[2]
-  rows=2
+  rows=1
   cols=2
   left = Inches(5.83)
   top = Inches(1.93)
